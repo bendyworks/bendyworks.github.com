391be30a111876e678e4c221ab14161e246c86a2
Bradley Grzesiak <brad@bendyworks.com>
2010-09-10 14:35:35 -0500

    Remove extraneous alert

--- presentations/2010-08-28-how-git-and-github-change-the-open-source-scene/file/strike.js
+++ presentations/2010-08-28-how-git-and-github-change-the-open-source-scene/file/strike.js
@@ -1,6 +1,5 @@
 $(function(){
   $(".foobarbaz").bind("showoff:next", function (event) {
-    alert('foo');
     var h1 = $(event.target).find("li");
     h1
       .fadeOut(300, function () { $(this).css({textDecoration: "line-through"}); })
